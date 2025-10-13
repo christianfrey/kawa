5418c5fc7b8032aab1d8b9c5b24c904441206289
Christian Frey <christian.ubu@gmail.com>
2025-10-13 14:19:34 +0200

    Fix duplicate icon issue by moving MenuBarManager to AppDelegate

--- Kawa/AppDelegate.swift
+++ Kawa/AppDelegate.swift
@@ -3,11 +3,16 @@ import SwiftUI
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    private var menuBarManager: MenuBarManager?
+    
     func applicationDidFinishLaunching(_ notification: Notification) {
         print("🎯 App finished launching")
         
         // Hide from dock
         NSApplication.shared.setActivationPolicy(.accessory)
+        
+        // Init menu bar manager
+        menuBarManager = MenuBarManager()
   
         // Request notification permissions with more comprehensive handling
         // UNUserNotificationCenter.current().requestAuthorization(
--- Kawa/KawaApp.swift
+++ Kawa/KawaApp.swift
@@ -4,11 +4,6 @@ import SwiftUI
 struct KawaApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var sleepManager = SleepPreventionManager.shared
-    @StateObject private var menuBarManager: MenuBarManager
-
-    init() {
-        self._menuBarManager = StateObject(wrappedValue: MenuBarManager())
-    }
 
     var body: some Scene {
         Settings {
